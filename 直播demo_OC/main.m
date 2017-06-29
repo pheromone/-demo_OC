952bbe41c34ad0945041604ec5ee5414d077ae90
周少停 <zst@9elelphas.com>
2017-06-29 13:43:15 +0800

    Initial Commit

---
+++ 直播demo_OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  直播demo_OC
+//
+//  Created by Shaoting Zhou on 2017/6/29.
+//  Copyright © 2017年 Shaoting Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
